aa7eab78ad8748b2c5b6ad1eff58c456e227fd18
Kaitlin <kayak0806@yahoo.com>
2014-02-07 17:26:03 -0500

    working on classes

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 # Main run file
+import math
 
 
-class Point:
+class Point():
     # Has an x coord (int) and a y coord (int)
-    # Get returns a tuple (x,y)
-    #!! distance takes a Point, returns the distance
+    # getPos returns a tuple (x,y)
+    # updatePos takes a tuple (x,y), changes x and y to match
+    # distance takes a Point, returns the distance
     
     def __init__(self,x,y):
       self.x = x
       self.y = y
       
-    def get(self):
+    def getPos(self):
         return (self.x,self.y)
+    def updatePos(self,x,y):
+        self.x = x
+        self.y = y
     
     def distance(self,point):
-    #!!
-        return 0 
+        dx = self.x - point.x
+        dy = self.y - point.y
+
+        dist = math.sqrt(dx**2 + dy**2)
+        return dist
         
         
     def __str__(self):
         return "(%s,%s)"%(self.x, self.y)
         
-class Item:
-    # Has a location (Point), a size (int), and a title (str)
-    # Get[x] returns x
-    
-    def __init__(self,location,size,title):
-        self.location = location
+class Item(Point):
+    # Has a location (Point), a size (int)
+    # getSize returns int size
+    # getType retunrs string type
+    # updateSize takes an int, updates the size
+    # updateType takes a string, updates the type
+
+    def __init__(self,x,y,size,type):
+        Point.__init__(self,x,y)
         self.size = size
-        self.title = title
+        self.type = type
         
-    def getLocation(self):
-        return self.location
     def getSize(self):
         return self.size
-    def getTitle(self):
-        return self.title
+    def getType(self):
+        return self.type
+    def updateSize(self,size):
+        self.size = size
+    def updateType(self,type):
+        self.type = type 
 
     def __str__(self):
-        first = self.title+" is located at "+str(self.location)
+        first = self.type+" is located at "+str(self.location)
         second =  " with radius %s"%(self.size)
         return first+second
 
 class Obsticle(Item):
     def __init__(self,location,size):
-        Item.init(location,size,"obsticle")
+        Item.__init__(self,location,size,"obsticle")
     
-    def distance
   
   
 
 def main():
     
-    myItem = Item(Point(1,1),2,"obsticle")
-    print myItem
+    myItem = Item(1,2,2,"obsticle")
+
+    print myItem.getPos()
     
     # Planned order of operations:
     #   recive info
